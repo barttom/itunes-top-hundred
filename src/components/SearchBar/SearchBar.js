4428a28c3d6f935a18ea64eef3bb2e576eec9bb5
Bart Tomczyk <bartlomiej.tomczyk@coderslab.pl>
2018-11-25 20:08:08 +0100

    Add searchbar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Search } from "react-feather";
+import "./_SearchBar.scss";
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textFilter: ""
+    };
+  }
+  changeHandler = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
   render() {
     return (
-      <section className="search">
-        <input type="text" />
+      <section className="search-bar">
+        <div className="search-bar__text">
+          <input
+            className="form-control"
+            type="text"
+            name="textFilter"
+            value={this.state.textFilter}
+            onChange={this.changeHandler}
+          />
+          <button className="btn btn-primary">
+            <Search />
+          </button>
+        </div>
       </section>
     );
   }
